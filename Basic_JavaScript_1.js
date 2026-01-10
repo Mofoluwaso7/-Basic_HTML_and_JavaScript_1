0d75c1c7103e7d8869134530d0e8832fc64120c0
Mofoluwaso7 <drfoluso@yahoo.com>
2026-01-10 15:48:56 +0100

    Update Basic_JavaScript_1.js

--- Basic_JavaScript_1.js
+++ Basic_JavaScript_1.js
@@ -1,4 +1,28 @@
 <!DOCTYPE html>
 <html>
     <script src="Hello_World.js"></script>
+    
+
+Below is pasted from the cut function of Basic_HTML_2.html file    
+   
+    </html>
+    <!DOCTYPE html>
+<html>
+<head>
+  <title>getElementById Without Function</title>
+</head>
+<body>
+
+  <!-- Paragraph with ID "Irish" -->
+  <p id="Irish">Hello from Ireland 🇮🇪</p>
+
+  <!-- Button element -->
+  <button onclick="alert(document.getElementById('Irish').innerText)">
+    Click Me
+  </button>
+
+</body>
 </html>
+
+
+
